c3206f2e27e56c3a4ed513ed9fc12c2da7274d22
Mikel <miguel-angel.ciordia@soprasteria.com>
2025-04-16 21:31:06 +0200

    set routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { ViewTasksComponent } from './navigation/view-tasks/view-tasks.component';
+import { SetTaskComponent } from './navigation/set-task/set-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'tasks', component: ViewTasksComponent },
+    { path: 'tasks/pending', component: ViewTasksComponent },
+    { path: 'tasks/create', component: SetTaskComponent },
+    { path: 'tasks/edit/:id', component: SetTaskComponent },
+    { path: '', redirectTo: '/tasks', pathMatch: 'full' }
+  ];
