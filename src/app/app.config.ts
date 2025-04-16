4cb9d34c43b65bfdc4f26a9e198e579c822285f4
Mikel <miguel-angel.ciordia@soprasteria.com>
2025-04-16 21:24:38 +0200

    material config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,16 @@
 import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 import { ApiModule, Configuration } from './api-client';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
+      BrowserModule,
+      BrowserAnimationsModule,
       ApiModule.forRoot(() => new Configuration({ basePath: 'http://localhost:8080' }))
     ),
     provideHttpClient(withInterceptorsFromDi()),
