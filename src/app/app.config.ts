279c44489b9b7bbe9233864c347c3f760902e179
Mikel <miguel-angel.ciordia@soprasteria.com>
2025-04-16 21:20:55 +0200

    api client config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,17 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { ApiModule, Configuration } from './api-client';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    importProvidersFrom(
+      ApiModule.forRoot(() => new Configuration({ basePath: 'http://localhost:8080' }))
+    ),
+    provideHttpClient(withInterceptorsFromDi()),
+    provideRouter(routes),
+    provideZoneChangeDetection({ eventCoalescing: true })
+  ]
 };
